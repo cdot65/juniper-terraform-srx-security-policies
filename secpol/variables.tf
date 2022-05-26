5c1698486cb3016794e7446a7c4edbeafc7d9c0b
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-05-26 11:00:37 +0000

    update defaults

--- secpol/variables.tf
+++ secpol/variables.tf
@@ -2,49 +2,49 @@
 variable "apply_group_name" {
   type        = string
   description = "Name of our apply-group for the apply group stanza"
-  // default     = "cdot65_ipsec"
+  // default     = "cdot65_sec_policy"
 }
 
 
 variable "from__zone__name" {
   type        = string
   description = "set security policy from zone {from__zone__name} to zone {to__zone__name}"
-  // default     = "cdot65_ike_gateway"
+  // default     = "ZONE_UNTRUST"
 }
 
 variable "to__zone__name" {
   type        = string
   description = "set security policy from zone {from__zone__name} to zone {to__zone__name}"
-  // default     = "56.0.0.1"
+  // default     = "ZONE_TRUST"
 }
 
 variable "policy_name" {
   type        = string
   description = "set security policy from zone {from__zone__name} to zone {to__zone__name} policy {policy_name}"
-  // default     = "ge-0/0/0.0"
+  // default     = "TF_SEC_POLICY1"
 }
 
 variable "description" {
   type        = string
   description = "set security policy from zone {from__zone__name} to zone {to__zone__name} policy {policy_name} description {description}"
-  // default     = "cdot65_ike_proposal"
+  // default     = "Test Security Policy 1"
 }
 
 variable "application" {
   type        = string
   description = "set security policy from zone {from__zone__name} to zone {to__zone__name} policy {policy_name} match application {application}"
-  // default     = "sha-256"
+  // default     = "any"
 }
 
 variable "source__address" {
   type        = string
   description = "set security policy from zone {from__zone__name} to zone {to__zone__name} policy {policy_name} match source-address {source__address}"
-  // default     = "pre-shared-keys"
+  // default     = "any"
 }
 
 variable "destination__address" {
   type        = string
   description = "set security policy from zone {from__zone__name} to zone {to__zone__name} policy {policy_name} match destination-address {destination__address}"
-  // default     = "DH14-aes-128-sha-256"
+  // default     = "any"
 }
 
